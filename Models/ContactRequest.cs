59d050158ccf50c2a04b8218acb38f5f45d9703e
Pirapakaran Neelakirushnan <UT011136@unicomtic.com>
2026-09-11 11:44:26 +0530

    Fix Contact Request module

--- Models/ContactRequest.cs
+++ Models/ContactRequest.cs
@@ -12,7 +12,6 @@
 
         public string Status { get; set; } = "Pending";
 
-        public DateTime CreatedAt { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
